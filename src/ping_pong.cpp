3bb64c0aab38e5e62a529bcaf0fbe0ec9bf240e5
Marian Triebe <m.triebe@live.de>
2015-08-03 23:18:37 +0200

    Fix compile

--- src/ping_pong.cpp
+++ src/ping_pong.cpp
@@ -18,9 +18,6 @@
 #include <string>
 #include <sstream>
 #include <iostream>
-#include <functional>
-
-#include "cppa/opt.hpp"
 
 #include "caf/all.hpp"
 #include "caf/io/all.hpp"
@@ -37,7 +34,7 @@ class ponger : public event_based_actor {
         return make_message(atom("pong"));
       },
       on(atom("pong")) >> [=] {
-        std::chrono::seconds pause(1);
+        chrono::seconds pause(1);
         this_thread::sleep_for(pause);
         return make_message(atom("pong"));
       },
@@ -45,79 +42,53 @@ class ponger : public event_based_actor {
         send(srv, atom("ping"));
       }
     };
-  } 
+  }
  private:
 };
 
-std::function<void()> verbose_exit;
-
-template <class T>
-void require(const optional<T>& arg, const char* arg_name) {
-  if (!arg) {
-    cerr << "*** " << arg_name << " missing" << endl;
-    verbose_exit();
-  }
-}
-
-void require(const std::string& arg, const char* arg_name) {
-  if (arg.empty()) {
-    cerr << "*** " << arg_name << " missing" << endl;
-    verbose_exit();
-  }
-}
-
-template <class T, class V, class... Vs>
-void require(T&& a0, const char* a1, V&& a2, const char* a3, Vs&&... as) {
-  require(std::forward<T>(a0), a1);
-  require(std::forward<V>(a2), a3, std::forward<Vs>(as)...);
-}
-
 int main(int argc, char** argv) {
-  std::string nexus_host;
-  optional<uint16_t> nexus_port;
+  string nexus_host = "localhost";
+  uint16_t nexus_port = 1345;
   bool is_server = false;
-  std::string host = "localhost";
-  optional<uint16_t> port;
-  options_description desc;
-  bool args_valid = match_stream<std::string>(argv + 1, argv + argc) (
-    on_opt1('N', "caf-nexus-host", &desc, "nexus host") >> rd_arg(nexus_host),
-    on_opt1('P', "caf-nexus-port", &desc, "nexus port") >> rd_arg(nexus_port),
-    on_opt0('h', "help", &desc, "print help") >> print_desc_and_exit(&desc),
-    on_opt1('H', "host", &desc, "calculator server host") >> rd_arg(host),
-    on_opt1('p', "port", &desc, "set server port") >> rd_arg(port),
-    on_opt0('S', "server", &desc, "run as server") >> set_flag(is_server)
-  );
-  verbose_exit = print_desc_and_exit(&desc, std::cerr, EXIT_FAILURE);
-  if (!args_valid) {
+  string host = "localhost";
+  uint16_t port = 1897;
+  auto res = message_builder(argv + 1, argv + argc).extract_opts({
+    {"caf-nexus-host,N", "nexus host (DEFAULT: localhost)", nexus_host},
+    {"caf-nexus-port,P", "nextus port (DEFAULT: 1345)", nexus_port},
+    {"help,h", "print help"},
+    {"host,H", "calculator server host (DEFAULT: localhost)", host},
+    {"port,p", "calculator server port (DEFAULT: 1897)", port},
+    {"server,S", "run as server", is_server}
+  });
+  if (res.opts.count("help") > 0) {
+    cerr << res.helptext << endl;
+  }
+  if (!res.error.empty()) {
     cerr << "*** invalid command line options" << endl;
-    verbose_exit();
+    cerr << res.error << endl;
+    cerr << res.helptext << endl;
   }
-  require(port, "port",
-          nexus_port, "nexus port",
-          nexus_host, "nexus host");
-  if (!is_server) {
-  }
-  if (!riac::init_probe(nexus_host, *nexus_port)) {
+  if (!riac::init_probe(nexus_host, nexus_port)) {
     cerr << "probe::init failed" << endl;
     return 1;
   }
   if (is_server) {
-    cout << "Starting server on port " << *port << endl;
+    cout << "Starting server on port " << port << endl;
     try {
       // try to publish math actor at given port
-      io::publish(spawn<ponger>(), *port);
+      io::publish(spawn<ponger>(), port);
     }
     catch (exception& e) {
-      cerr << "*** unable to publish math actor at port " << *port << endl
+      cerr << "*** unable to publish math actor at port " << port << endl
            << to_verbose_string(e) << endl;
       return EXIT_FAILURE;
     }
   } else {
     cout << "Starting client and connecting to "
-         << host << " on port " << *port << endl;
+         << host << " on port " << port << endl;
     scoped_actor self;
     auto cli = spawn<ponger>();
-    auto srvr = io::remote_actor(host, *port);
+    auto srvr = io::remote_actor(host, port);
     self->send(cli, atom("init"), srvr);
   }
   await_all_actors_done();
--- src/probed_calculator.cpp
+++ src/probed_calculator.cpp
@@ -18,9 +18,6 @@
 #include <string>
 #include <sstream>
 #include <iostream>
-#include <functional>
-
-#include "cppa/opt.hpp"
 
 #include "caf/all.hpp"
 #include "caf/io/all.hpp"
@@ -41,82 +38,52 @@ void calculator(event_based_actor* self) {
   );
 }
 
-std::function<void()> verbose_exit;
-
-template <class T>
-void require(const optional<T>& arg, const char* arg_name) {
-  if (!arg) {
-    cerr << "*** " << arg_name << " missing" << endl;
-    verbose_exit();
-  }
-}
-
-void require(const std::string& arg, const char* arg_name) {
-  if (arg.empty()) {
-    cerr << "*** " << arg_name << " missing" << endl;
-    verbose_exit();
-  }
-}
-
-template <class T, class V, class... Vs>
-void require(T&& a0, const char* a1, V&& a2, const char* a3, Vs&&... as) {
-  require(std::forward<T>(a0), a1);
-  require(std::forward<V>(a2), a3, std::forward<Vs>(as)...);
-}
-
 int main(int argc, char** argv) {
-  std::string nexus_host;
-  optional<uint16_t> nexus_port;
+  string nexus_host = "localhost";
+  uint16_t nexus_port = 1345;
   bool is_server = false;
-  std::string host = "localhost";
-  optional<uint16_t> port;
-  options_description desc;
-  optional<int> x;
-  optional<int> y;
-  bool args_valid = match_stream<std::string>(argv + 1, argv + argc) (
-    on_opt1('N', "caf-nexus-host", &desc, "nexus host") >> rd_arg(nexus_host),
-    on_opt1('P', "caf-nexus-port", &desc, "nexus port") >> rd_arg(nexus_port),
-    on_opt0('h', "help", &desc, "print help") >> print_desc_and_exit(&desc),
-    on_opt1('H', "host", &desc, "calculator server host") >> rd_arg(host),
-    on_opt1('p', "port", &desc, "set server port") >> rd_arg(port),
-    on_opt0('S', "server", &desc, "run as server") >> set_flag(is_server),
-    on_opt1('x', "x-value", &desc, "set X value", "client options") >> rd_arg(x),
-    on_opt1('y', "y-value", &desc, "set Y value", "client options") >> rd_arg(y)
-  );
-  verbose_exit = print_desc_and_exit(&desc, std::cerr, EXIT_FAILURE);
-  if (!args_valid) {
+  string host = "localhost";
+  uint16_t port = 1897;
+  auto res = message_builder(argv + 1, argv + argc).extract_opts({
+    {"caf-nexus-host,N", "nexus host (DEFAULT: localhost)", nexus_host},
+    {"caf-nexus-port,P", "nexus port (DEFAULT: 1345)", nexus_port},
+    {"help,h", "print help"},
+    {"host,H", "calculator server host (DEFAULT: localhost)", host},
+    {"port,p", "set server port (DEFAULT: 1897)", port},
+    {"server,S", "run as server", is_server},
+    {"x-value,x", "set X value"},
+    {"y-value,y", "set Y value"}
+  });
+  if (!res.error.empty()) {
     cerr << "*** invalid command line options" << endl;
-    verbose_exit();
+    cerr << res.error << endl;
+    cerr << res.helptext << endl;
   }
-  require(port, "port",
-          nexus_port, "nexus port",
-          nexus_host, "nexus host");
-  if (!is_server) {
-    require(x, "x", y, "y");
-  }
-  if (!riac::init_probe(nexus_host, *nexus_port)) {
+  int x = res.opts.count("x-value");
+  int y = res.opts.count("y-value");
+  if (!riac::init_probe(nexus_host, nexus_port)) {
     cerr << "probe::init failed" << endl;
     return 1;
   }
   if (is_server) {
-    cout << "Starting server on port " << *port << endl;
+    cout << "Starting server on port " << port << endl;
     try {
       // try to publish math actor at given port
-      io::publish(spawn(calculator), *port);
+      io::publish(spawn(calculator), port);
     }
     catch (exception& e) {
-      cerr << "*** unable to publish math actor at port " << *port << endl
+      cerr << "*** unable to publish math actor at port " << port << endl
            << to_verbose_string(e) << endl;
       return EXIT_FAILURE;
     }
   } else {
     cout << "Starting client and connecting to "
-         << host << " on port " << *port << endl;
+         << host << " on port " << port << endl;
     scoped_actor self;
-    auto srvr = io::remote_actor(host, *port);
-    self->sync_send(srvr, atom("plus"), *x, *y).await(
+    auto srvr = io::remote_actor(host, port);
+    self->sync_send(srvr, atom("plus"), x, y).await(
       on(atom("result"), arg_match) >> [&] (int result) {
-        cout << *x << " + " << *y << " = " << result << endl;
+        cout << x << " + " << y << " = " << result << endl;
       }
     );
   }
